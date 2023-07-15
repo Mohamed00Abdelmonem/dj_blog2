ed51e48f1b63b0e16e7eedf9b7ecaba1f1201a54
unknown <mmohamedabdelm@gmail.com>
2023-07-15 13:23:37 +0300

    show all post in index page

--- post/views.py
+++ post/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from .models import Post
 # Create your views here.
+
+def list_post(request):
+    data = Post.objects.all()
+    return render(request,'post_list.html', {'context': data})
